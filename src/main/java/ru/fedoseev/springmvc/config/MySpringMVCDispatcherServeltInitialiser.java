f8f2fa799e3a2b43346f2f8331c2404f5d8a010a
Matvey Fedoseev <41521530+MFedoss@users.noreply.github.com>
2020-12-12 05:40:40 +0300

    Update MySpringMVCDispatcherServeltInitialiser.java

--- src/main/java/ru/fedoseev/springmvc/config/MySpringMVCDispatcherServeltInitialiser.java
+++ src/main/java/ru/fedoseev/springmvc/config/MySpringMVCDispatcherServeltInitialiser.java
@@ -11,11 +11,11 @@ public class MySpringMVCDispatcherServeltInitialiser extends AbstractAnnotationC
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[] {SpringConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] {"/"};
     }
 }
