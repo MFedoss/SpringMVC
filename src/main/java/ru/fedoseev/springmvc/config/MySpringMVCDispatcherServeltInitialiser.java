8f97ca71d9cbeefa340401eb10ab7bccdcfe2d22
Matvey Fedoseev <41521530+MFedoss@users.noreply.github.com>
2020-12-12 05:37:06 +0300

    Update MySpringMVCDispatcherServeltInitialiser.java

--- src/main/java/ru/fedoseev/springmvc/config/MySpringMVCDispatcherServeltInitialiser.java
+++ src/main/java/ru/fedoseev/springmvc/config/MySpringMVCDispatcherServeltInitialiser.java
@@ -6,7 +6,7 @@ public class MySpringMVCDispatcherServeltInitialiser extends AbstractAnnotationC
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
